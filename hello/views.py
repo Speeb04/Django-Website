c100e6eb6b0ba4b0a3de58b6010e0e50a54530e5
Speeb04 <sz111706@outlook.com>
2025-11-25 17:18:57 -0500

    changed homepage

--- hello/views.py
+++ hello/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, world! Your Django site is running.")
+    return render(request, 'home.html', {'title': 'Speebsite'})
